a9758403f9992c68adbc4fc943f291673af2aa0c
seacrest <cassiep1986@gmail.com>
2021-07-26 16:30:16 -0700

    formatted

--- routes/api.js
+++ routes/api.js
@@ -2,64 +2,68 @@ const router = require("express").Router();
 const workout = require("../models/workout.js");
 
 router.post("/api/workouts", ({ body }, res) => {
-  workout.create(body)
-    .then(dbworkout => {
+  workout
+    .create(body)
+    .then((dbworkout) => {
       res.json(dbworkout);
     })
-    .catch(err => {
+    .catch((err) => {
       res.status(400).json(err);
     });
 });
 
 router.get("/api/workouts", (req, res) => {
-  workout.aggregate( [
-    {
-      $addFields: {
-        totalDuration: { $sum: "$exercises.duration" } ,
-        
-      }
-    } 
- ] )
+  workout
+    .aggregate([
+      {
+        $addFields: {
+          totalDuration: { $sum: "$exercises.duration" },
+        },
+      },
+    ])
     .sort({ date: -1 })
-    .then(dbworkout => {
+    .then((dbworkout) => {
       res.json(dbworkout);
     })
-    .catch(err => {
+    .catch((err) => {
       res.status(400).json(err);
     });
 });
 
 router.put("/api/workouts/:id", (req, res) => {
-  workout.updateOne({_id:req.params.id}, {
-    $push:{
-      exercises: req.body
-    }
-  })
-    .then(dbworkout => {
+  workout
+    .updateOne(
+      { _id: req.params.id },
+      {
+        $push: {
+          exercises: req.body,
+        },
+      }
+    )
+    .then((dbworkout) => {
       res.json(dbworkout);
     })
-    .catch(err => {
+    .catch((err) => {
       res.status(400).json(err);
     });
 });
 
 router.get("/api/workouts/range", (req, res) => {
-  workout.aggregate( [
-    {
-      $addFields: {
-        totalDuration: { $sum: "$exercises.duration" } ,
-        
-      }
-    } 
- ] )
+  workout
+    .aggregate([
+      {
+        $addFields: {
+          totalDuration: { $sum: "$exercises.duration" },
+        },
+      },
+    ])
     .sort({ date: -1 })
-    .then(dbworkout => {
+    .then((dbworkout) => {
       res.json(dbworkout);
     })
-    .catch(err => {
+    .catch((err) => {
       res.status(400).json(err);
     });
 });
-
 
 module.exports = router;
